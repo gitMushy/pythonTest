13e2d69321014e3a32a530941140a610a05012cc
gitMushy <94109471+gitMushy@users.noreply.github.com>
2021-11-14 21:06:09 +0000

    Created first.py
    just first file to test project

---
+++ first.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+class firstGit:
+  
+  def __init__(self):
+    pass
+  
+  def pt(self,messageToPrint):
+    print(messageToPrint)
+   
+ 
+
+fG = firstGit()
+fG.pt("hello first git")
